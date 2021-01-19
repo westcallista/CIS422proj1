310837ad92ca0eb8b18fb96132b9d13e693760c7
Logan Levitre <loganlevitre@gmail.com>
2021-01-18 23:06:52 -0800

    Wrote files rough draft header and imported pandas as pd

--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,15 @@
-import pandas as pd
+"""
+File: preprocessing.py
+Class: CIS 422
+Date: January 18, 2021
+Team: The Nerd Herd
+Head Programmer: Logan Levitre
+Version 0.0.0
 
+Overview: Preprocessing functions to be used with Time Series Data.
+    -- Need second opinion for revision of header structure --
+"""
+import pandas as pd
+# -- Dummy code for now - used to get rid of warnings while initial pandas importing and API reading
+pd.read_csv("dummycall.csv")
+pd.Series.rolling()
