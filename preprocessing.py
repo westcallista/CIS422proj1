2506bd74e59105be28ad42d8b682d81d9eebbe35
Logan Levitre <loganlevitre@gmail.com>
2021-01-19 17:35:39 -0800

    Created skeleton of preprocessing functions

--- preprocessing.py
+++ preprocessing.py
@@ -4,12 +4,78 @@ Class: CIS 422
 Date: January 18, 2021
 Team: The Nerd Herd
 Head Programmer: Logan Levitre
-Version 0.0.0
+Version 0.1.0
 
 Overview: Preprocessing functions to be used with Time Series Data.
     -- Need second opinion for revision of header structure --
 """
 import pandas as pd
-# -- Dummy code for now - used to get rid of warnings while initial pandas importing and API reading
-pd.read_csv("dummycall.csv")
-pd.Series.rolling()
+
+
+def read_from_file(input_file):
+    pass
+
+
+def write_to_file(output_file):
+    pass
+
+
+def denoise(time_series):
+    pass
+
+
+def impute_missing_data(time_series):
+    pass
+
+
+def impute_outliers(time_series):
+    pass
+
+
+def longest_continuous_run(time_series):
+    pass
+
+
+def clip(time_series, starting_date, final_date):
+    pass
+
+
+def assign_time(time_series, start, increment):
+    pass
+
+
+def difference(time_series):
+    pass
+
+
+def scaling(time_series):
+    pass
+
+
+def standardize(time_series):
+    pass
+
+
+def logarithm(time_series):
+    pass
+
+
+def cubic_roots(time_series):
+    pass
+
+
+def split_data(time_series, perc_training, perc_valid, perc_test):
+    pass
+
+
+def design_matrix(time_series, input_index, output_index):
+    pass
+
+
+def design__matrix(time_series, m_i, t_i, m_0, t_0):
+    pass
+
+
+def ts2db(input_file, perc_training, perc_valid, perc_test, input_index,
+          output_index, output_file):
+    pass
