eca203e5f2c9f1014852b84ad03a7d1b9c3a70a9
jarettn18 <jarettn18@gmail.com>
2021-01-20 18:30:54 -0800

    Initialization for model.py

---
+++ model.py
@@ -0,0 +1,30 @@
+"""
+File: model.py
+Class: CIS 422
+Date: January 20, 2021
+Team: The Nerd Herd
+Head Programmer: Jarett Nishijo
+Version 0.1.0
+
+Overview: Modeling functions to be used with preprocessed
+        data from preprocessing.py
+"""
+
+class mlp_model():
+
+    def __init__(self, input_dimension, output_dimension [, layers]):
+        pass
+
+    def fit(self, x_train, y_train):
+        pass
+
+class rf_model():
+
+    def __init__(self):
+        pass
+
+    def fit(self, x_train, y_train):
+        pass
+
+    def forecast(self, x):
+        pass
